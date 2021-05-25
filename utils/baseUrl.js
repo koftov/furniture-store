70cd956deadefa410108b65c6cd9688473f9a07c
Aharon Lubitsch <59685123+ahronlu@users.noreply.github.com>
2021-05-26 00:10:20 +0300

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://furniture-store-pi.vercel.app'
+    ? 'https://bike-store.vercel.app'
     : 'http://localhost:3000';
 
 export default baseUrl;
